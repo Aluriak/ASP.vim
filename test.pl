af1a82d264e5d43b0256f600fb594e4d9eaf5009
Aluriak <lucas.bourneuf@laposte.net>
2015-04-04 12:50:50 +0200

    ASP code instead of prolog code

--- test.pl
+++ test.pl
@@ -1,81 +1,23 @@
-% A small Prolog file to test prolog.vim
-%
-%
+%*
+ * A small ASP file to test ASP.vim
+ * Mainly found these lines on Wikipedia
+ *%
 
-head :- body.
 
-:- use_module  foobar(faoeu[aoeu]).
-:- dynamic foobar/2.
+% graph colouring
+c(1..n).                                           
+1 {color(X,I) : c(I)} 1 :- v(X).             
+:- color(X,I), color(Y,I), e(X,Y), c(I).
 
-:- statement.
-:- statement(x,y,_X,Y,foo(x,Y)).
-?- query.
-?- query(x,y,_X,Y,foo(x,Y)).
 
-head(_) :- body(foo,Foo,foo(foo),_foo).
+% hamiltonian circle
+{in(X,Y)} :- e(X,Y).
+ 
+:- 2 {in(X,Y) : e(X,Y)}, v(X).
+:- 2 {in(X,Y) : e(X,Y)}, v(Y).
 
-head((foobar,baz)booh,X,foo(booh,X)) :- body.
-head(booh,X,foo(booh,X)) :- body(foo,Foo,foo(foo),_foo).
+r(X) :- in(0,X), v(X).
+r(Y) :- r(X), in(X,Y), e(X,Y).
 
-head --> body >>.
-head --> body(foo,Foo,foo(foo)).
+:- not r(X), v(X).
 
-head(booh,X,foo(booh,X)) --> body.
-head(booh,X,foo(booh,X)) --> foo,
-	body(foo,Foo,foo(foo)), {foo(bar)}.
-
-:- dynamic foo/2.
-
-foobar.
-
-foobar :- !, \+ ,  =@= =:= << >>.
-
-head :- foo(C), foo(B),
-	/* something commentary 
-	*
-	* */
-
-	% more comments
-	\+ foo(G).
-
-/* a multiline
-* comment
-*/
-
-% another multiline comment
-% consisting of a block of normal comments
-
-head :- Error(foo), _error(bar). /*predicates must start lower-case*/
-
-interpret((Index,Old),New,World) :-
-	foobar (
-; uaoeu
-).
-
-head :- (error(1); aoeuoe
-;  aoeu
-	oouaoeu
-;
-	aoeuaoe
-	/*
-	* aoeuaoe
-	*/
-;
-	aoeu
-% aoeuae
-% aoeu
-)error(2). /*no comma and a TODO*/
-
-Error
-_error
-
-head :- body, Ts =:= =.. [Foo|Bar].
-
-head :- list([a,b,C,foo:Bar|Tail],Foo).
-head :- dlist(Huuu-Ts).
-
-head :- foobar('string').
-head :- format('fooo ~n bar').
-
-% numbers
-head :- X is 2 + 3.4.
